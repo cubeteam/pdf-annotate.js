dcdfb3ee229d4d063de86caaf4e8d6f584dcd3da
Dima <dmitryl0626@gmail.com>
2021-03-18 16:13:42 +0100

    web.bat fix

--- scripts/web.bat
+++ scripts/web.bat
@@ -2,6 +2,6 @@ echo y | copy .\node_modules\pdfjs-dist\build\pdf.js .\shared
 echo y | copy .\node_modules\pdfjs-dist\build\pdf.worker.js .\shared
 echo y | copy .\node_modules\pdfjs-dist\build\pdf_viewer.js .\shared
 echo y | copy .\node_modules\pdfjs-dist\build\pdf_viewer.css .\shared
-echo y | copy .\node_modules\pdfjs-dist\cmaps .\shared
+echo y | Xcopy .\node_modules\pdfjs-dist\cmaps .\shared\cmaps\
 
 .\node_modules\.bin\webpack-dev-server --inline --config .\webpack.web.js --content-base web
